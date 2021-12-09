1d56b6bc8039fe8a106751b398db6d71e949a7c5
César Matías Romero <cesarmatiromero@gmail.com>
2021-12-09 20:25:28 -0300

    Queda por terminar el ejercicio 10

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 // Solicitar al usuario su nombre y mostrarlo por alert. Luego Mostrar un alert que salude diciendo Hola y el nombre de la persona. Luego solicitar el apellido y que salude por alert con el nombre completo (Ej: Hola Florencia Andres).
 
 let nombre = prompt("¿Cual es tu nombre?");
@@ -56,39 +57,61 @@ let tercerNumero = prompt("Ingrese un número");
 let resultadoFinal = parseFloat(tercerNumero)*parseFloat(resultado);
 alert("Su resultado es: "+resultadoFinal); 
 
+
 // Armar un conversor de minutos a segundos. Solicitar al usuario un número de minutos y mostrar por alert la cantidad de segundos a la que equivale.
 alert("Conversor de minutos a segundos");
-let numeroMin = prompt("Ingrese un número en minutos: ");
+let numeroMin = prompt("Ingrese la cantidadde minutos que quiere convertir: ");
 let segundos = numeroMin*60;
-alert("Son segundos: " +segundos)
+alert("Son segundos: " +segundos);
+
+
+// Solicitar al usuario la medida de la base y de la altura de un rectángulo y devolver el área.
+alert("Por favor ingresa la medidas de base y altura de un rectángulo.");
+let base = prompt("Ingresa la medida de la base");
+let altura = prompt("Ingresa la medida de la altura");
+let area = parseFloat(base)*parseFloat(altura);
+alert("El area del rectángulo es: "+area);
+
+
+
+// Solicitar al usuario la medida de la base y de la altura de un triángulo equilátero y devolver el área.
+alert("Ingresa las medidas de base y altura de un triángulo equilátero.");
+let base = prompt("Ingresa la medida de la base");
+let altura = prompt("Ingresa la medida de la altura");
+let area = (parseFloat(base)*parseFloat(altura))/2;
+alert("El area del triángulo equilatero es: "+area);
+*/
+
+// Solicitar al usuario un monto y un número de descuento. Mostrar al usuario cuánto es el precio final con el descuento aplicado.
+let monto = prompt("Ingresa un monto de dinero");
+let descuento = prompt("Ingresa el descuento en % que se le realizara a tu compra");
+let precioFinal = parseFloat(monto)
+
 
 
 
 /*
-8) Solicitar al usuario la medida de la base y de la altura de un rectángulo y devolver el área.
+// Solicitar al usuario su nombre y la edad que cumple o cumplió en 2021, y mostrar un mensaje con alert diciendo "Hola (nombre) usted nació en el año (año)"
 
-9) Solicitar al usuario la medida de la base y de la altura de un triángulo equilátero y devolver el área.
+// Crear un conversor de Celsius a Farenheit. Solicitar al usuario su nombre y una temperatura y mostrar por alert su equivalente en Farenheit junto con un mensaje que incluya ambos datos. 
 
-10) Solicitar al usuario un monto y un número de descuento. Mostrar al usuario cuánto es el precio final con el descuento aplicado.
+// Realizar el mismo ejercicio que el anterior pero al revés, de Farenheit a Celsius.
 
-11) Solicitar al usuario su nombre y la edad que cumple o cumplió en 2021, y mostrar un mensaje con alert diciendo "Hola (nombre) usted nació en el año (año)"
-
-12) Crear un conversor de Celsius a Farenheit. Solicitar al usuario su nombre y una temperatura y mostrar por alert su equivalente en Farenheit junto con un mensaje que incluya ambos datos. 
-
-13) Realizar el mismo ejercicio que el anterior pero al revés, de Farenheit a Celsius.
-
-14) Solicitar al usuario un número y almacenarlo en una variable. Sumarle 5 , multiplicar el resultado por 10 y sacar el resto de su división por 3. Mostrar el resultado sin comas.
+// Solicitar al usuario un número y almacenarlo en una variable. Sumarle 5 , multiplicar el resultado por 10 y sacar el resto de su división por 3. Mostrar el resultado sin comas.
 
 
-BONUS:
+//BONUS:
 
-0) Solicitar dos números y mostrar la suma de sus cifras. (Ejemplo: 15 y 29, mostrar 1529).
+// Solicitar dos números y mostrar la suma de sus cifras. (Ejemplo: 15 y 29, mostrar 1529).
 
-1) Crear un conversor de años a segundos. Solicitar al usuario una cantidad de días y mostrar su equivalente en segundos por alert o alert, a elección. (Pueden probar también con números con coma :) 2,5 años por ejemplo)
+// Crear un conversor de años a segundos. Solicitar al usuario una cantidad de días y mostrar su equivalente en segundos por alert o alert, a elección. (Pueden probar también con números con coma :) 2,5 años por ejemplo)
 
-2) Solicitar al usuario que ingrese la cantidad de kilómetros que desea recorrer con su motocicleta. A continuación solicitar la velocidad promedio a la que desea ir en kilómetros por hora. Calcular el tiempo estimado en llegar a destino en minutos.  A tener en cuenta: la velocidad se calcula en metros sobre segundos!)
+// Solicitar al usuario que ingrese la cantidad de kilómetros que desea recorrer con su motocicleta. A continuación solicitar la velocidad promedio a la que desea ir en kilómetros por hora. Calcular el tiempo estimado en llegar a destino en minutos.  A tener en cuenta: la velocidad se calcula en metros sobre segundos!)
 
-3) Realizar una calculadora de sueldo neto. Se solicita ingresar el sueldo bruto y se descuenta: Obra social 3%, jubilación 11%, Ley 19032 3%. Mostrar el monto de cada una de las deducciones y el monto final. No contempla impuesto a las ganancias ni sindicatos.
+
+// Realizar una calculadora de sueldo neto. Se solicita ingresar el sueldo bruto y se descuenta: Obra social 3%, jubilación 11%, Ley 19032 3%. Mostrar el monto de cada una de las deducciones y el monto final. No contempla impuesto a las ganancias ni sindicatos.
+
+
 ej: Sueldo bruto: $70.000
     Deducciones:
     Obra social: $2100
@@ -96,6 +119,11 @@ ej: Sueldo bruto: $70.000
    Jubilación: $7700
    Neto a pagar: $58100
    */
+
+
+
+
+
 
 
 
