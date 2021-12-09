159dcfad8ae04b8d71ecb07a17f35b52ef805aeb
César Matías Romero <cesarmatiromero@gmail.com>
2021-12-09 20:36:22 -0300

    Se agregaron ejercicion de condicionales

---
+++ Ejercicios Condicionales JavaScript/app.js
@@ -0,0 +1,12 @@
+/* EJERCICIOS
+Solicitar al usuario que ingrese un texto y validar si contiene la letra “a”.
+Solicitar al usuario que ingrese un texto y mostrarlo todo en minúsculas y todo en mayúsculas.
+Solicitar al usuario que ingrese una palabra y mostrar con qué letra comienza.
+Solicitar al usuario que ingrese una frase y mostrar cuántos caracteres tiene.
+Solicitar a un usuario que ingrese un número y devolver cuántos dígitos tiene.
+Solicitar al usuario que ingrese una palabra y mostrar con qué letra termina.
+Ingresar una cadena de texto y reemplazar los espacios por asteriscos.
+Solicitar al usuario una letra y una cadena de texto e indicar si contiene la letra indicada.
+Solicitar un texto y mostrarlo todo menos la primera letra.
+Solicitar al usuario que ingrese su nombre en minúscula y mostrarlo con la primera letra mayúscula.
+*/
